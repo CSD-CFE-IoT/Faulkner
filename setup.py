814ca29856f612a6bd0bc704a4d675e9ba3c1a9c
Nicolas Ontiveros <niontive@microsoft.com>
2019-10-11 15:16:11 -0700

    Launch simulation environment with one roslaunch file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['turtlebot3_teleop'],
+    packages=['faulkner'],
     package_dir={'': 'src'}
 )
 
